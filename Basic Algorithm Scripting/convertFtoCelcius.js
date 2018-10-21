b529e69914e1bc2b51f0c7f3ec95c7628f3d6227
jon-0 <jonathanotano@icloud.com>
2018-10-21 12:12:01 -0400

    moved files into folder

---
+++ Basic Algorithm Scripting/convertFtoCelcius.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+convertToF(30);
+// convertToF(-30) should return a value of -22
+// convertToF(-10) should return a value of 14
+// convertToF(0) should return a value of 32
+// convertToF(20) should return a value of 68
+// convertToF(30) should return a value of 86
