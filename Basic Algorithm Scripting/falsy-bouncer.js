7eb13771884df796f51b178369063fbdb4eabd5d
jon-0 <jonathanotano@icloud.com>
2018-10-25 05:53:19 -0400

    Update Basic Algorithm Scripting/falsy-bouncer.js

---
+++ Basic Algorithm Scripting/falsy-bouncer.js
@@ -0,0 +1,18 @@
+// Remove all falsy values from an array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  var passed = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i]) {
+      passed.push(arr[i]);
+    }
+  }
+  return passed;
+}
+
+bouncer([7, "ate", "", false, 9]) // should return [7, "ate", 9].
+bouncer(["a", "b", "c"]) // should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]) // should return [].
+bouncer([1, null, NaN, 2, undefined]) // should return [1, 2].
