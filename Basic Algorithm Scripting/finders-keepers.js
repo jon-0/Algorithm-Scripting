b49640f95534f904c16cb752e054027d81667209
jon-0 <jonathanotano@icloud.com>
2018-10-24 21:19:20 -0400

    Update Basic Algorithm Scripting/finders-keepers.js

--- Basic Algorithm Scripting/finders-keepers.js
+++ Basic Algorithm Scripting/finders-keepers.js
@@ -1,4 +1,6 @@
-// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+// Create a function that looks through an array (first argument) and returns 
+// the first element in the array that passes a truth test (second argument). 
+// If no element passes the test, return undefined.
 
 function findElement(arr, func) {
   for (var i = 0; i < arr.length; i++) {
